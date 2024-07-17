842c6fe1807cfc9f869ea01b765a6e1654c9b38d
Brian Lesko <brianjosephlesko@gmail.com>
2024-07-16 22:02:18 -0400

    Update test_pi.py

--- test_pi.py
+++ test_pi.py
@@ -2,8 +2,6 @@ import streamlit as st
 import arduino as ard
 
 st.title("Control Input")
-power = st.slider("Power", min_value=0, max_value=100, value=0)
-angle = st.slider("Angle", min_value=0, max_value=180, value=95)
 
 if "my_arduino" not in st.session_state:
     port = '/dev/ttyACM0'
@@ -13,16 +11,18 @@ if "my_arduino" not in st.session_state:
         st.write(f"Connection to {port} successful")
     except Exception as e:
         st.write("Could not connect to arduino")
-else:
-    st.write(f"Connection to arduino successful")
 
-throttle = 'throttle:'+str(power)
-steering = 'servo:'+str(angle)
+if "my_arduino" in st.session_state:
+    power = st.slider("Power", min_value=0, max_value=100, value=0)
+    angle = st.slider("Angle", min_value=0, max_value=180, value=95)
 
-# Send Serial Signal 
-try:
-    st.session_state.my_arduino.send(throttle)
-    st.session_state.my_arduino.send(steering)
-except Exception as e:
-    st.write("Error occurred while sending the serial signal.")
-    st.write(e)
+    throttle = 'throttle:'+str(power)
+    steering = 'servo:'+str(angle)
+
+    # Send Serial Signal 
+    try:
+        st.session_state.my_arduino.send(throttle)
+        st.session_state.my_arduino.send(steering)
+    except Exception as e:
+        st.write("Error occurred while sending the serial signal.")
+        st.write(e)
