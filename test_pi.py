e751659b510901a5f3352a3e930875a477675ca9
Brian Lesko <brianjosephlesko@gmail.com>
2024-07-16 21:58:52 -0400

    Update test_pi.py

--- test_pi.py
+++ test_pi.py
@@ -5,16 +5,20 @@ st.title("Control Input")
 power = st.slider("Power", min_value=0, max_value=100, value=0)
 angle = st.slider("Angle", min_value=0, max_value=180, value=95)
 
-port = '/dev/ttyACM0'
-try: 
-    BAUD = 9600
-    my_arduino = ard.arduino(port,BAUD,.1)
-    st.write(f"Connection to {port} successful")
-except Exception as e:
-    st.write("Could not connect to arduino")
+if "my_arduino" not in st.session_state:
+    port = '/dev/ttyACM0'
+    try: 
+        BAUD = 9600
+        my_arduino = ard.arduino(port,BAUD,.1)
+        st.write(f"Connection to {port} successful")
+    except Exception as e:
+        st.write("Could not connect to arduino")
+else:
+    my_arduino = st.session_state.my_arduino
+    st.write(f"Connection to arduino successful")
 
-    throttle = 'throttle:'+str(power)
-    steering = 'servo:'+str(angle)
+throttle = 'throttle:'+str(power)
+steering = 'servo:'+str(angle)
 
 # Send Serial Signal 
 try:
